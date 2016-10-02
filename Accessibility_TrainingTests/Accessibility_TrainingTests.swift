eccfb6c48d3264be21c92074e9f119b9ddf45017
Will Richardson <will@grinfulapps.com>
2016-10-01 22:22:58 -0500

    Initial Commit

---
+++ Accessibility_TrainingTests/Accessibility_TrainingTests.swift
@@ -0,0 +1,36 @@
+//
+//  Accessibility_TrainingTests.swift
+//  Accessibility_TrainingTests
+//
+//  Created by Will Richardson on 10/1/16.
+//  Copyright © 2016 Will Richardson. All rights reserved.
+//
+
+import XCTest
+@testable import Accessibility_Training
+
+class Accessibility_TrainingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
